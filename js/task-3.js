495ea3f5d9dcb2a381998be226538619af1b21d6
Alina-Vitiuk <vityukalina@gmail.com>
2024-06-18 18:56:04 +0300

    commit1

---
+++ js/task-3.js
@@ -0,0 +1,18 @@
+"strict mode";
+function checkForSpam(message) {
+  const word1 = "spam";
+  const word2 = "sale";
+  message = message.toLowerCase();
+  return (
+    message.includes(word1.toLowerCase()) ||
+    message.includes(word2.toLowerCase())
+  );
+}
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
